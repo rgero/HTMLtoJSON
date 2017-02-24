ab3e72297c9628870496fce91315cc82f82c4354
Roy Gero <roy.gero.ii@gmail.com>
2017-02-24 17:02:04 -0500

    Implemented the Links.

--- HTMLtoJSON.py
+++ HTMLtoJSON.py
@@ -13,9 +13,9 @@ class MyHTMLParser(HTMLParser):
         self.docDic["pageContent"] = []
         self.parentTag = None # This will only get set if it encounters a table, list or link.
         self.lastTag = None
-        self.openRow = False
         self.currentItem = None
         self.currentTag = None
+        self.linkString = None # This... ugh. Links.
 
     def handle_starttag(self, tag, attrs):
         tag = tag.lower()
@@ -45,16 +45,18 @@ class MyHTMLParser(HTMLParser):
             elif tag == "li":
                 self.currentTag = "li"
 
-            elif tag == "table" and self.parentTag == None:
+            elif tag == "table":
                 self.parentTag = tag
                 self.currentItem = {"table" : []}
-            elif tag == "tr":
-                self.openRow = True
             elif tag == "td":
                 self.currentTag = tag
                 if self.lastTag != "td":
                     self.lastTag = tag
                     self.currentItem["table"].append(["row", []])
+        if tag == "a":
+            link = attrs[0][1]
+            self.linkString = "<a href=" + link + " target='_blank'>"
+
 
 
 
@@ -63,21 +65,26 @@ class MyHTMLParser(HTMLParser):
         if tag == "td":
             self.currentTag = None
         if tag == "tr":
-            self.openRow = False
             self.lastTag = None
         if tag == "a":
-            pass
+            self.linkString += "</a>"
+            if "paragraph" in self.currentItem.keys():
+                self.currentItem["paragraph"] += self.linkString
+            elif "table" in self.currentItem.keys():
+                self.currentItem["table"][len(self.currentItem["table"])-1][1].append(self.linkString)
+            self.linkString = None
         elif self.currentTag == tag or self.parentTag == tag:
             self.currentTag = None
             self.parentTag = None
             self.docDic["pageContent"].append(self.currentItem)
+            print(self.currentItem)
             self.currentItem = None
 
     def handle_data(self, data):
         if self.currentTag != None:
             if self.currentTag == "h3":
                 self.currentItem["heading"] = data.rstrip()
-            elif self.currentTag == "p":
+            elif self.currentTag == "p" and self.linkString == None:
                 self.currentItem["paragraph"] += data.strip()
             elif self.currentTag == "li":
                 if self.parentTag == "ol":
@@ -90,10 +97,11 @@ class MyHTMLParser(HTMLParser):
                 self.currentItem["table"][len(self.currentItem["table"])-1][1].append(data.rstrip())
             elif self.currentTag == "a" and self.lastTag == "td":
                 self.currentItem["table"][len(self.currentItem["table"])-1][1].append(data.rstrip())
+        if self.linkString != None:
+            self.linkString += data.rstrip()
 
 file = open('guitar-trainer.html', 'r')
 data = file.read()
 
 parser = MyHTMLParser()
 parser.feed(data)
-print(parser.docDic)
